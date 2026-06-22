223a4a42e0296b6389700574de53ac55b3388a25
paramjitbaral <2300033156@kluniversity.in>
2026-06-23 03:03:58 +0530

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Block all console output for security and cleanliness
+if (typeof window !== 'undefined') {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
